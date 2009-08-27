f40b905621fe6ea39385255fa76cee7f534a22c7
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-08-27 13:40:33 -0400

    added type sigs so haddock would print documentation

--- Text/Bio/PDB/Misc.hs
+++ Text/Bio/PDB/Misc.hs
@@ -3,6 +3,7 @@ module Text.Bio.PDB.Misc where
 import Text.ParserCombinators.Parsec
 
 -- | Platform (Windows, *nix) agnostic line terminator
+eol :: GenParser Char st String
 eol =     try (string "\n\r")
       <|> try (string "\r\n")
       <|> string "\n"
@@ -10,7 +11,9 @@ eol =     try (string "\n\r")
       <?> "end of line"
 
 -- | quick & dirty integer
+integer :: GenParser Char st String
 integer = many digit
 
 -- | PDB serial numbers
+serialNumber :: GenParser Char st String
 serialNumber = integer
